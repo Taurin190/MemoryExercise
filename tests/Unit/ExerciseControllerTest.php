2f69af589aee2ddcab0ec0f9cacab586459e07e1
Koichi Taura <taura.koichi@gmail.com>
2020-01-30 23:43:07 +0900

    add failure test for controllers

--- tests/Unit/ExerciseControllerTest.php
+++ tests/Unit/ExerciseControllerTest.php
@@ -5,9 +5,17 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Http\Controllers\ExerciseController;
 
 class ExerciseControllerTest extends TestCase
 {
+    private $ExerciseController;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->ExerciseController = new ExerciseController();
+    }
     /**
      * A basic unit test example.
      *
@@ -16,5 +24,29 @@ class ExerciseControllerTest extends TestCase
     public function testExample()
     {
         $this->assertTrue(true);
+    }
+
+    public function testIndex()
+    {
+        $actual = $this->ExerciseController->index();
+        $this->assertSame(view("exercise_index"), $actual);
+    }
+
+    public function testList()
+    {
+        $actual = $this->ExerciseController->list();
+        $this->assertSame(view("exercise_list"), $actual);
+    }
+
+    public function testForm()
+    {
+        $actual = $this->ExerciseController->form();
+        $this->assertSame(view("exercise_form"), $actual);
+    }
+
+    public function testCreate()
+    {
+        $actual = $this->ExerciseController->create();
+        $this->assertSame(view("exercise_create"), $actual);
     }
 }
