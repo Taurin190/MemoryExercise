5e251326a2e89e1982046de5669e93a51358c190
Koichi Taura <taura.koichi@gmail.com>
2020-02-05 22:10:16 +0900

    modify question create form

--- resources/views/exercise_form.blade.php
+++ resources/views/exercise_form.blade.php
@@ -1,23 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <div class="container">
+        <form class="form-horizontal">
+            <div class="form-group">
+                <label class="col-sm-2 control-label" for="InputName">Title</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="InputName" placeholder="タイトル">
                 </div>
             </div>
-        </div>
+            <div class="form-group">
+                <label class="col-sm-2 control-label" for="InputQuestion">Question</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="InputQuestion" placeholder="質問">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-2 control-label" for="InputAnswer">Answer</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="InputAnswer" placeholder="答え">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                    <button type="submit" class="btn btn-primary btn-block">送信</button>
+                </div>
+            </div>
+        </form>
     </div>
-</div>
 @endsection
