330a4b98e464ba8b8cb485b8bbb325f647ae184d
Koichi Taura <taura.koichi@gmail.com>
2020-02-07 23:37:33 +0900

    add form feature to function to get form value

--- app/Http/Controllers/ExerciseController.php
+++ app/Http/Controllers/ExerciseController.php
@@ -31,8 +31,27 @@ class ExerciseController extends Controller
         return view('exercise_list');
     }
 
+    /**
+     * when post from form
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create()
     {
+        if (Request::has('InputName')) {
+            $name = Request::input('InputName');
+        } else {
+
+        }
+        if (Request::has('InputQuestion')) {
+            $question = Request::input('InputQuestion');
+        } else {
+
+        }
+        if (Request::has('InputAnswer')) {
+            $answer = Request::input('InputAnswer');
+        } else {
+
+        }
         return view('exercise_create');
     }
 
