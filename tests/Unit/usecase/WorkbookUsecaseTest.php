5cdc93dcb633b057dd772ff3ad9c6bde4d21bfea
Koichi Taura <taura.koichi@gmail.com>
2020-04-25 07:39:48 +0900

    add irregular case to create workbook

--- tests/Unit/usecase/WorkbookUsecaseTest.php
+++ tests/Unit/usecase/WorkbookUsecaseTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\usecase;
 
 use App\Usecase\WorkbookUsecase;
+use App\Usecase\WorkbookDomainException;
 use Tests\TestCase;
 
 use \Mockery as m;
@@ -32,7 +33,29 @@ class WorkbookUsecaseTest extends TestCase
 
         $workbook = new WorkbookUsecase($this->workbookRepositoryMock);
         $actual = $workbook->createWorkbook("test workbook", "This is test workbook.");
-        self::assertSame($actual, 1);
+        self::assertSame( 1, $actual);
+    }
+
+    public function testCreateWorkbookWithEmptyDescription()
+    {
+        $this->workbookRepositoryMock->shouldReceive('add')->with("test workbook", "")->once()->andReturn(1);
+
+        $workbook = new WorkbookUsecase($this->workbookRepositoryMock);
+        $actual = $workbook->createWorkbook("test workbook", "");
+        self::assertSame( 1, $actual);
+    }
+
+    public function testCreateWorkbookWithEmptyTitle()
+    {
+        $this->workbookRepositoryMock->shouldReceive('add')->with("", "This is test workbook.")->once()->andThrow(new WorkbookDomainException("タイトルが空です。"));
+
+        $workbook = new WorkbookUsecase($this->workbookRepositoryMock);
+        try {
+            $actual = $workbook->createWorkbook("", "This is test workbook.");
+            self::fail('Exceptionが投げられませんでした。');
+        } catch (WorkbookDomainException $e) {
+            $this->assertSame("タイトルが空です。", $e->getMessage());
+        }
     }
 
     public function testModifyWorkbook()
@@ -41,6 +64,6 @@ class WorkbookUsecaseTest extends TestCase
 
         $workbook = new WorkbookUsecase($this->workbookRepositoryMock);
         $actual = $workbook->modifyWorkbook(1, "test workbook", "This is test workbook.");
-        self::assertSame($actual, 1);
+        self::assertSame(1, $actual);
     }
 }
