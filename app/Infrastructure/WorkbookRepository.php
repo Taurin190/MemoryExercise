0f24ba64bafc8dcb6261c172a532899e19d46679
Koichi Taura <taura.koichi@gmail.com>
2020-04-29 22:02:49 +0900

    add implementation about findby id and its test on workbook repository

--- app/Infrastructure/WorkbookRepository.php
+++ app/Infrastructure/WorkbookRepository.php
@@ -4,22 +4,17 @@ use App\Domain\Workbook;
 
 class WorkbookRepository implements \App\Domain\WorkbookRepository
 {
-    private $model;
-
-    function __construct(\App\Workbook $workbook_model)
-    {
-        $this->model = $workbook_model;
-    }
-
-    function save(Workbook $workbook)
-    {
-    }
-
     function findByWorkbookId(int $workbook_id)
     {
+        $model = \App\Workbook::find($workbook_id);
+        return Workbook::map($model->first());
     }
 
     function findAll()
+    {
+    }
+
+    function save(Workbook $workbook)
     {
     }
 
---
+++ tests/Unit/infrastructure/WorkbookRepositoryTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace Tests\Unit\infrastructure;
+use \App\Infrastructure\WorkbookRepository;
+use Tests\TestCase;
+use \Mockery as m;
+
+class WorkbookRepositoryTest extends TestCase
+{
+    protected $workbookDomainMock;
+    protected $workbookMock;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->workbookDomainMock = m::mock('alias:\App\Domain\Workbook');
+        $this->workbookMock = m::mock('alias:\App\Workbook');
+    }
+    public function testFindByWorkbookId()
+    {
+        $this->workbookMock
+            ->shouldReceive('find')
+            ->once()
+            ->with(1)
+            ->andReturn($this->workbookMock);
+        $this->workbookMock
+            ->shouldReceive('first')
+            ->once()
+            ->andReturn($this->workbookMock);
+        $this->workbookDomainMock
+            ->shouldReceive('map')
+            ->once()
+            ->with($this->workbookMock)
+            ->andReturn($this->workbookDomainMock);
+        $repository = new WorkbookRepository();
+        $domain = $repository->findByWorkbookId(1);
+        self::assertTrue($domain instanceof \App\Domain\Workbook);
+    }
+
+    public function testFindAll() {
+        $repository = new WorkbookRepository();
+        $actual = $repository->findAll();
+    }
+}
