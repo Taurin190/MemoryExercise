49f6d2fb06aa1705ca1f404de9a3d150cbb249a4
Koichi Taura <taura.koichi@gmail.com>
2020-05-05 15:51:25 +0900

    add description when there is no data of workbook

--- resources/views/workbook_list.blade.php
+++ resources/views/workbook_list.blade.php
@@ -4,6 +4,9 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            @if(count($workbooks) == 0)
+                問題集が存在しません。
+            @endif
             @foreach($workbooks as $workbook)
             <div class="card">
                 <a href="/workbook/{{$workbook->getWorkbookId()}}">
