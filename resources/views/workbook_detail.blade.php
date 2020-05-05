83b5d79a88a009a1981d8d5be4836daaa3b20d79
Koichi Taura <taura.koichi@gmail.com>
2020-05-05 16:55:20 +0900

    delete unnecessary blade description

--- resources/views/workbook_detail.blade.php
+++ resources/views/workbook_detail.blade.php
@@ -4,19 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            {{--@if(count($workbooks) == 0)--}}
-                {{--問題集が存在しません。--}}
-            {{--@endif--}}
-            {{--@foreach($workbooks as $workbook)--}}
-            {{--<div class="card">--}}
-                {{--<a href="/workbook/{{$workbook->getWorkbookId()}}">--}}
-                {{--<div class="card-header">{{ $workbook->getTitle() }}</div>--}}
-                {{--<div class="card-body">--}}
-                        {{--{{ $workbook->getExplanation() }}--}}
-                {{--</div>--}}
-                {{--</a>--}}
-            {{--</div>--}}
-            {{--@endforeach--}}
+            <h2>{{ $workbook->getTitle() }}</h2>
+            @if(!empty($workbook->getExplanation()))
+                <span>{{ $workbook->getExplanation() }}</span>
+            @endif
+
         </div>
     </div>
 </div>
