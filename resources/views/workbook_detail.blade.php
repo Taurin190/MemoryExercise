db8a79c6cd1485de36c1a3fdf70cb9113d74c2b7
Koichi Taura <taura.koichi@gmail.com>
2020-05-06 05:29:20 +0900

    add self check button and submit button

--- resources/views/workbook_detail.blade.php
+++ resources/views/workbook_detail.blade.php
@@ -10,12 +10,27 @@
             @endif
             @foreach($workbook->getExerciseList() as $exercise)
                 <p>{{ $exercise->getQuestion() }}</p>
-                <a data-toggle="collapse" href="#collapse-{{$exercise->getExerciseId()}}">解答を表示</a>
-                <div id="collapse-{{$exercise->getExerciseId()}}" class="collapse card">
-                    <div class="card-body">{{ $exercise->getAnswer() }}</div>
+                <div>
+                    <a data-toggle="collapse" href="#collapse-{{$exercise->getExerciseId()}}">解答を表示</a>
+                    <div id="collapse-{{$exercise->getExerciseId()}}" class="collapse card">
+                        <div class="card-body">{{ $exercise->getAnswer() }}</div>
+                    </div>
+                </div>
+                <div class="btn-group btn-group-toggle" data-toggle="buttons" role="group" aria-label="{{ $exercise->getExerciseId() }}">
+                    <label class="btn btn-info">
+                        <input type="radio" autocomplete="off" checked>覚えた</input>
+                    </label>
+                    <label class="btn btn-info">
+                        <input type="radio" autocomplete="off">ちょっと怪しい</input>
+                    </label>
+                    <label class="btn btn-info">
+                        <input type="radio" autocomplete="off">分からない</input>
+                    </label>
                 </div>
             @endforeach
-            <div></div>
+            <div>
+                <button class="btn btn-primary btn-block">回答完了</button>
+            </div>
         </div>
     </div>
 </div>
