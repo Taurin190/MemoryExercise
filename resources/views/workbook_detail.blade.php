f19fa48e0698c74e3b1b47647d4052136ad04941
Koichi Taura <taura.koichi@gmail.com>
2020-05-07 06:20:53 +0900

    modify workbook detail ui

--- resources/views/workbook_detail.blade.php
+++ resources/views/workbook_detail.blade.php
@@ -4,31 +4,41 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h2>{{ $workbook->getTitle() }}</h2>
+            <h1>{{ $workbook->getTitle() }}</h1>
             @if(!empty($workbook->getExplanation()))
-                <span>{{ $workbook->getExplanation() }}</span>
+                <div class="card">
+                    <div class="card-body">{{ $workbook->getExplanation() }}</div>
+                </div>
             @endif
+            @php($problem_count = 0)
             {{Form::open(['route' => [ 'workbook.complete', $workbook->getWorkbookId()]])}}
             @foreach($workbook->getExerciseList() as $exercise)
-                <p>{{ $exercise->getQuestion() }}</p>
-                <div>
-                    <a data-toggle="collapse" href="#collapse-{{$exercise->getExerciseId()}}">解答を表示</a>
-                    <div id="collapse-{{$exercise->getExerciseId()}}" class="collapse card">
-                        <div class="card-body">{{ $exercise->getAnswer() }}</div>
+                @php($problem_count++)
+                <div class="py-4">
+                    <b>問題{{$problem_count}}</b>
+                    <p>{{ $exercise->getQuestion() }}</p>
+                    <div class="pb-4">
+                        <a data-toggle="collapse" href="#collapse-{{$exercise->getExerciseId()}}">解答を表示</a>
+                        <div id="collapse-{{$exercise->getExerciseId()}}" class="collapse card">
+                            <div class="card-body">
+                                <b>解答</b>
+                                {{ $exercise->getAnswer() }}
+                            </div>
+                        </div>
                     </div>
+                    <div class="btn-group btn-group-toggle" data-toggle="buttons" role="group" aria-label="{{ $exercise->getExerciseId() }}">
+                        <label class="btn btn-info">
+                            <input type="radio" autocomplete="off" name="{{$exercise->getExerciseId()}}" value="ok">覚えた</input>
+                        </label>
+                        <label class="btn btn-info">
+                            <input type="radio" autocomplete="off" name="{{$exercise->getExerciseId()}}" value="studying">ちょっと怪しい</input>
+                        </label>
+                        <label class="btn btn-info active">
+                            <input type="radio" autocomplete="off" name="{{$exercise->getExerciseId()}}" value="ng" checked required>分からない</input>
+                        </label>
+                    </div>
+                    <input type="hidden" name="exercise_list[]" value="{{$exercise->getExerciseId()}}" />
                 </div>
-                <div class="btn-group btn-group-toggle" data-toggle="buttons" role="group" aria-label="{{ $exercise->getExerciseId() }}">
-                    <label class="btn btn-info">
-                        <input type="radio" autocomplete="off" name="{{$exercise->getExerciseId()}}" value="ok">覚えた</input>
-                    </label>
-                    <label class="btn btn-info">
-                        <input type="radio" autocomplete="off" name="{{$exercise->getExerciseId()}}" value="studying">ちょっと怪しい</input>
-                    </label>
-                    <label class="btn btn-info active">
-                        <input type="radio" autocomplete="off" name="{{$exercise->getExerciseId()}}" value="ng" checked required>分からない</input>
-                    </label>
-                </div>
-                <input type="hidden" name="exercise_list[]" value="{{$exercise->getExerciseId()}}" />
             @endforeach
             <div>
                 <button class="btn btn-primary btn-block">回答完了</button>
