7468a793bfc3cd6d46790548156ac33e5cd5bca9
Koichi Taura <taura.koichi@gmail.com>
2020-05-07 07:13:46 +0900

    add vue object for workbook

---
+++ resources/js/components/WorkbookComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="col-md-8">
+        <h1>{{ workbook.title }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "WorkbookComponent",
+        props:{
+            workbook: Object
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
