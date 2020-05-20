730a23117bbf47bf66c6f769ef69d865fa3a0438
Koichi Taura <taura.koichi@gmail.com>
2020-05-21 06:35:33 +0900

    add empty exercise usecase class and test

---
+++ app/Usecase/ExerciseUsecase.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: koichi.taura
+ * Date: 2020/05/21
+ * Time: 6:32
+ */
+
+namespace App\Usecase;
+use App\Http\Requests\ExerciseRequest;
+
+class ExerciseUsecase
+{
+    public function createExerciseFromRequest(ExerciseRequest $request) {
+
+    }
+}
