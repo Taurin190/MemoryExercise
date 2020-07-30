b93c43ac9d29fa6c4f574991b5a67a80ef119f07
Koichi Taura <taura.koichi@gmail.com>
2020-07-30 17:55:14 +0900

    implement a bit ExerciseHistory domain

--- app/Domain/ExerciseHistory.php
+++ app/Domain/ExerciseHistory.php
@@ -11,5 +11,24 @@ namespace App\Domain;
 
 class ExerciseHistory
 {
+    private $exercise_history_id;
 
+    private $score;
+
+    private $user;
+
+    private $exercise;
+
+    public static function create($user, $exercise, $score) {
+        return new ExerciseHistory($user, $exercise, $score);
+    }
+
+    private function __construct($user, $exercise, $score, $exercise_history_id = null) {
+        $this->user = $user;
+        $this->exercise = $exercise;
+        $this->score = $score;
+        if (isset($exercise_history_id)) {
+            $this->exercise_history_id = $exercise_history_id;
+        }
+    }
 }
