430789e075a0d18c22dbc7ae62eb9a06415688c1
Koichi Taura <taura.koichi@gmail.com>
2020-08-12 18:14:35 +0900

    update index page design

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="banner-image">
+        <div class="container align-middle">
+            <div class="row justify-content-center banner-container">
+                <p class="banner-text">自分で作成した問題集で、</p>
+                <p class="banner-text">どこでも繰り返し学習</p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-8">
+                <h2>問題を自分で作成する事で記憶定着を促す</h2>
+            </div>
+        </div>
+    </div>
+@endsection
