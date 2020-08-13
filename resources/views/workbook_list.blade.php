69b83c2f7bb9efcd0b8f705ad7a1f1c6e4c356e5
Koichi Taura <taura.koichi@gmail.com>
2020-08-13 15:52:47 +0900

    update workbook design

--- resources/views/workbook_list.blade.php
+++ resources/views/workbook_list.blade.php
@@ -3,20 +3,27 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12 py-2">
             @if(count($workbooks) == 0)
                 問題集が存在しません。
-            @endif
+            @else
+            <div>
+                <span>{{ count($workbooks) }} 件</span>
+            </div>
             @foreach($workbooks as $workbook)
-            <div class="card">
-                <a href="/workbook/{{$workbook->getWorkbookId()}}">
-                <div class="card-header">{{ $workbook->getTitle() }}</div>
-                <div class="card-body">
+            <div class="col-md-3 py-2 px-1 float-left">
+                <div class="card workbook-card">
+                    <a class="text-body text-decoration-none" href="/workbook/{{$workbook->getWorkbookId()}}">
+                    {{--<div class="card-header"></div>--}}
+                    <div class="card-body">
+                        <h4 class="card-title">{{ $workbook->getTitle() }}</h4>
                         {{ $workbook->getExplanation() }}
+                    </div>
+                    </a>
                 </div>
-                </a>
             </div>
             @endforeach
+            @endif
         </div>
     </div>
 </div>
