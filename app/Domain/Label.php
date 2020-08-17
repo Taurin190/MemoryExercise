a27a8bfa99c317f61a515199d13098fd847a2b4f
Koichi Taura <taura.koichi@gmail.com>
2020-08-17 16:57:31 +0900

    add label and unit test for label

---
+++ app/Domain/Label.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: koichi.taura
+ * Date: 2020/08/17
+ * Time: 16:39
+ */
+
+namespace App\Domain;
+
+
+class Label
+{
+    private $label_id;
+
+    private $name;
+
+    private function __construct($name, $label_id = null)
+    {
+        $this->name = $name;
+        $this->label_id = $label_id;
+    }
+    public static function create($name)
+    {
+        return new Label($name);
+    }
+    public static function map(\App\Label $model)
+    {
+        return new Label($model->getAttribute("name"), $model->getKey());
+    }
+    public function getLabelId()
+    {
+        return $this->label_id;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+}
