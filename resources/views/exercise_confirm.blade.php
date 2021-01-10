6ecc7bc736dd2a5795ef193ad0a0c4e43961e4e1
Koichi Taura <taura.koichi@gmail.com>
2021-01-11 04:22:29 +0900

    improve exercise confirm page design

--- resources/views/exercise_confirm.blade.php
+++ resources/views/exercise_confirm.blade.php
@@ -4,20 +4,20 @@
 <div class="container">
     {{Form::open(['route' => [ 'exercise.complete']])}}
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <p>以下を作成してよろしいですか？</p>
+        <div class="card col-md-10 py-5 px-5">
+            <p>以下の内容を作成してよろしいですか？</p>
             <div>
-                <label>問題</label>
+                <label class="pt-2 my-2 form-label control-label">問題</label>
                 <p>{{ $exercise->getQuestion() }}</p>
                 <input type="hidden" name="question" value="{{ $exercise->getQuestion() }}">
             </div>
             <div>
-                <label>解答</label>
+                <label class="pt-2 my-2 form-label control-label">解答</label>
                 <p>{{ $exercise->getAnswer() }}</p>
                 <input type="hidden" name="answer" value="{{ $exercise->getAnswer() }}">
             </div>
-            <div>
-                <label>公開設定</label>
+            <div class="mb-3">
+                <label class="pt-2 my-2 form-label control-label">公開設定</label>
                 @if ($exercise->getPermission() === 1)
                 <p>公開</p>
                 <input type="hidden" name="permission" value="{{ $exercise->getPermission() }}">
@@ -26,13 +26,13 @@
                 <input type="hidden" name="permission" value="{{ $exercise->getPermission() }}">
                 @endif
             </div>
+            <div class="col-md-12 px-0 btn-group d-flex" role="group" aria-label="...">
+                <a href="{{route('exercise.create')}}"
+                   class="btn btn-outline-secondary px-0 col-md-3 float-left">戻る</a>
+                <input type="submit"
+                       class="btn btn-outline-primary px-0 mx-2 float-left col-md-3" value="作成" />
+            </div>
         </div>
-    </div>
-    <div class="btn-group d-flex pb-2" role="group" aria-label="...">
-        <a href="{{route('exercise.create')}}"
-                class="btn btn-outline-secondary w-100">戻る</a>
-        <input type="submit"
-                class="btn btn-outline-primary w-100" value="作成" />
     </div>
     {{Form::close()}}
 </div>
