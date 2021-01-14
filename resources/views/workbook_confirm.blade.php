d4c6a1f4df8ed35d459a4d28878f0f97d1d90d4a
Koichi Taura <taura.koichi@gmail.com>
2021-01-14 18:21:48 +0900

    improve design for workbook confirm and complete

--- resources/views/workbook_confirm.blade.php
+++ resources/views/workbook_confirm.blade.php
@@ -4,21 +4,21 @@
 <div class="container">
     {{Form::open(['route' => [ 'workbook.complete']])}}
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <p>以下を作成してよろしいですか？</p>
+        <div class="card col-md-10 py-5 px-5">
+            <p>以下の内容を作成してよろしいですか？</p>
             <div>
-                <label>問題集のタイトル</label>
+                <label class="pt-2 my-2 form-label control-label">問題集のタイトル</label>
                 <p>{{ $workbook->getTitle() }}</p>
                 <input type="hidden" name="title" value="{{ $workbook->getTitle() }}">
             </div>
             <div>
-                <label>問題集の説明</label>
+                <label class="pt-2 my-2 form-label control-label">問題集の説明</label>
                 <p>{!! nl2br(e($workbook->getExplanation())) !!}</p>
                 <input type="hidden" name="explanation" value="{{ $workbook->getExplanation() }}">
             </div>
             @isset($exercise_list)
             <div>
-                <label>問題</label>
+                <label class="pt-2 my-2 form-label control-label">問題</label>
                 <ul>
                     @foreach($exercise_list as $exercise)
                         <li>{{ $exercise->getQuestion() }}</li>
@@ -27,13 +27,13 @@
                 </ul>
             </div>
             @endisset
+            <div class="col-md-12 px-0 py-2 btn-group d-flex" role="group" aria-label="...">
+                <a href="{{route('workbook.create')}}"
+                   class="btn btn-outline-secondary px-0 col-md-3 float-left">戻る</a>
+                <input type="submit"
+                       class="btn btn-outline-primary px-0 mx-2 float-left col-md-3" value="作成" />
+            </div>
         </div>
-    </div>
-    <div class="btn-group d-flex pb-2" role="group" aria-label="...">
-        <a href="{{route('workbook.create')}}"
-           class="btn btn-outline-secondary w-100">戻る</a>
-        <input type="submit"
-               class="btn btn-outline-primary w-100" value="作成" />
     </div>
     {{Form::close()}}
 </div>
