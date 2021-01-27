2e40c5e9dc1baa03c62d73107d491365d2ce9cee
Koichi Taura <taura.koichi@gmail.com>
2021-01-28 08:35:57 +0900

    improve exercise page changing interface

--- resources/js/components/WorkbookComponent.vue
+++ resources/js/components/WorkbookComponent.vue
@@ -8,13 +8,30 @@
             <div v-if="getExerciseCount == 0">
                 <p class="py-3">問題が登録されていません。</p>
             </div>
+            <div class="py-5">
+                <button type="button"
+                    v-on:click="nextPage"
+                    class="btn btn-outline-info w-100">スタート</button>
+            </div>
         </div>
         <div v-else-if="page <= workbook.exercise_list.length">
             <div v-for="(exercise, index) in workbook.exercise_list">
                 <div v-show="page - 1 == index">
                     <h2>問題{{index + 1}}</h2>
-                    <p class="py-3 question">{{ exercise.question }}</p>
-                    <div class="pb-3">
+                    <a class="text-decoration-none" v-on:click="prevPage">
+                        <div class="workbook-prev">
+                            <i class="fa fa-angle-left prev-button" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                    <a class="text-decoration-none" v-on:click="nextPage">
+                        <div class="workbook-next">
+                            <i class="fa fa-angle-right next-button" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                    <div class="question-container">
+                        <p class="py-3 question">{{ exercise.question }}</p>
+                    </div>
+                    <div class="pb-3 answer-container">
                         <a data-toggle="collapse" v-bind:href="'#collapse-' + exercise.exercise_id">解答を表示</a>
                         <div v-bind:id="'collapse-' + exercise.exercise_id" class="collapse card">
                             <div class="card-body">
@@ -25,13 +42,13 @@
                     </div>
                     <div class="btn-group btn-group-toggle py-3 px-0 col-md-12" data-toggle="buttons" role="group" :aria-label="exercise.exercise_id">
                         <label class="btn btn-info col-md-4">
-                            <input type="radio" autocomplete="off" :name="exercise.exercise_id" value="ok"/>覚えた
+                            <input type="radio" v-on:click="nextPage" autocomplete="off" :name="exercise.exercise_id" value="ok"/>覚えた
                         </label>
                         <label class="btn btn-info col-md-4">
-                            <input type="radio" autocomplete="off" :name="exercise.exercise_id" value="studying"/>ちょっと怪しい
+                            <input type="radio" v-on:click="nextPage" autocomplete="off" :name="exercise.exercise_id" value="studying"/>ちょっと怪しい
                         </label>
-                        <label class="btn btn-info active col-md-4">
-                            <input type="radio" autocomplete="off" :name="exercise.exercise_id" value="ng" checked required/>分からない
+                        <label class="btn btn-info col-md-4">
+                            <input type="radio" v-on:click="nextPage" autocomplete="off" :name="exercise.exercise_id" value="ng" required/>分からない
                         </label>
                     </div>
                     <input type="hidden" name="exercise_list[]" :value="exercise.exercise_id" />
@@ -39,16 +56,6 @@
             </div>
         </div>
         <div class="pager-block">
-            <div v-if="getExerciseCount > 0" class="btn-group d-flex" role="group" aria-label="...">
-                <button type="button"
-                        v-on:click="prevPage"
-                        class="btn btn-outline-info w-100"
-                        v-bind:class="{ 'disabled' : isFirstPage }">前へ</button>
-                <button type="button"
-                        v-on:click="nextPage"
-                        class="btn btn-outline-info w-100"
-                        v-bind:class="{ 'disabled' : isFinalPage }">次へ</button>
-            </div>
             <div v-if="isFinalPage" class="pt-5">
                 <input v-if="getExerciseCount > 0" type="submit" class="btn btn-primary btn-block" value="回答完了"/>
                 <buttn v-else type="button" onclick="history.back()" class="btn btn-outline-secondary btn-block">戻る</buttn>
