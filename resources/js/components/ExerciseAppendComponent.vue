22d267914dd3c70237cc4652cfa14b08c33f0b26
Koichi Taura <taura.koichi@gmail.com>
2021-02-02 22:46:24 +0900

    modify array to object list to affect loaded exercises

--- resources/js/components/ExerciseAppendComponent.vue
+++ resources/js/components/ExerciseAppendComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div v-for="(exercise, index) in exercise_list" class="col-md-3 py-2 px-1 float-left">
+        <div v-for="(exercise, index) in exercise_objects" class="col-md-3 py-2 px-1 float-left">
             <div class="card workbook-card">
                 <a class="text-body text-decoration-none" :href="'/exercise/' + exercise.exercise_id ">
                     <div class="card-body">
@@ -8,8 +8,7 @@
                         <p class="text-break explanation">{{ exercise.answer }}</p>
                     </div>
                 </a>
-                <!--@if(isset($user_id) && exercise.user.id == $user_id)-->
-                <a class="text-decoration-none" :href="'/exercise/' + exercise.exercise_id + '/edit/'">
+                <a v-if="user_id === exercise.author_id" class="text-decoration-none" :href="'/exercise/' + exercise.exercise_id + '/edit/'">
                     <div class="workbook-edit">
                         <i class="fas fa-pen"></i>
                     </div>
@@ -25,22 +24,24 @@
         name: "ExerciseAppendComponent",
         props: {
             count : Number,
-            exercise_list: {}
+            exercise_list: {},
+            user_id: Number
         },
         data: function() {
             return {
-                // exercise_list: {},
+                exercise_objects: {},
                 page: 1
             }
         },
         mounted() {
             console.log($(document).innerHeight());
             console.log($(document).innerHeight() - $(window).innerHeight());
+            console.log(this.exercise_list);
             document.addEventListener('scroll', this.onScroll);
+            for (var i = 0; i < this.exercise_list.length; i ++) {
+                this.$set(this.exercise_objects, i, this.exercise_list[i]);
+            }
         },
-        // created () {
-        //     window.addEventListener('scroll', this.onScroll)
-        // },
         destroyed () {
             window.removeEventListener('scroll', this.onScroll)
         },
@@ -50,18 +51,17 @@
                 let doc_height = $(document).innerHeight();
                 let win_height = $(window).innerHeight();
                 let bottom = doc_height - win_height;
-                // console.log(bottom * 0.8);
                 console.log($(window).scrollTop());
-                if (bottom * 0.8 < $(window).scrollTop()) return;
+                if (bottom < $(window).scrollTop()) return;
                 this.page += 1;
                 axios({
-                    url: '/api/exercise/list?page=' + this.page,
+                    url: '/api/exercise/list?limit=12&page=' + this.page,
                     method: 'GET'
                 }).then(res =>  {
                     console.log(res);
                     for (var i = 0; i < res.data.exercise_list.length; i ++) {
-                        let index = this.exercise_list.length;
-                        this.$set(this.exercise_list, index, res.data.exercise_list[i]);
+                        let index = Object.keys(this.exercise_objects).length;
+                        this.$set(this.exercise_objects, index, res.data.exercise_list[i]);
                     }
                 })
             }
--- resources/views/exercise_index.blade.php
+++ resources/views/exercise_index.blade.php
@@ -7,29 +7,10 @@
             @if(count($exercise_list) == 0)
                 問題集が存在しません。
             @else
-            <div>
-               <span>{{ $count }} 件</span>
-            </div>
-            {{--@foreach($exercise_list as $exercise)--}}
-                {{--<div class="col-md-3 py-2 px-1 float-left">--}}
-                    {{--<div class="card workbook-card">--}}
-                        {{--<a class="text-body text-decoration-none" href="{{route('exercise.detail', $exercise->exercise_id)}}">--}}
-                            {{--<div class="card-body">--}}
-                                {{--<h4 class="card-title">{{ $exercise->question }}</h4>--}}
-                                {{--<p class="text-break explanation">{!! nl2br(e($exercise->answer)) !!}</p>--}}
-                            {{--</div>--}}
-                        {{--</a>--}}
-                        {{--@if(isset($user_id) && $exercise->user->id == $user_id)--}}
-                            {{--<a class="text-decoration-none" href="{{route('exercise.edit', $exercise->exercise_id)}}">--}}
-                                {{--<div class="workbook-edit">--}}
-                                    {{--<i class="fas fa-pen"></i>--}}
-                                {{--</div>--}}
-                            {{--</a>--}}
-                        {{--@endif--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--@endforeach--}}
-                <exercise-append-component :exercise_list='@json($exercise_list)' :count='@json($count)'></exercise-append-component>
+                <div>
+                   <span>{{ $count }} 件</span>
+                </div>
+                <exercise-append-component :user_id='@json($user_id)' :exercise_list='@json($exercise_list)' :count='@json($count)'></exercise-append-component>
             @endif
         </div>
     </div>
