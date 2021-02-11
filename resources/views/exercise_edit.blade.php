25ae2decbb7d9c057cbd439071efa837e795c600
Koichi Taura <taura.koichi@gmail.com>
2021-02-12 08:34:13 +0900

    add delete modal on workbook and exercise pages

--- resources/views/exercise_edit.blade.php
+++ resources/views/exercise_edit.blade.php
@@ -18,7 +18,7 @@
          <div class="row justify-content-center">
              <div class="card col-md-10 py-5 px-5">
                  <h2 class="mb-2"><i class="fas fa-question-circle pr-2"></i>問題編集</h2>
-                 <a class="text-decoration-none">
+                 <a class="text-decoration-none" data-toggle="modal" data-target="#deleteModal">
                      <div class="exercise-delete">
                          <i class="far fa-trash-alt delete-icon"></i>
                      </div>
@@ -84,5 +84,24 @@
              </div>
          </div>
         {{Form::close()}}
+            <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">削除しますか？</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            {{ $exercise->getQuestion() }}
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-light" data-dismiss="modal">キャンセル</button>
+                            <button type="button" class="btn btn-danger">削除</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </div>
 @endsection
--- resources/views/workbook_edit.blade.php
+++ resources/views/workbook_edit.blade.php
@@ -6,7 +6,7 @@
         <div class="card py-5 px-5 col-md-10">
             {{Form::open(['route' => [ 'workbook.edit.exercise', $workbook->getWorkbookId()]])}}
             <h2 class="mb-2"><i class="fas fa-book-open pr-2"></i>問題集編集</h2>
-            <a class="text-decoration-none">
+            <a class="text-decoration-none" data-toggle="modal" data-target="#deleteModal">
                 <div class="exercise-delete">
                     <i class="far fa-trash-alt delete-icon"></i>
                 </div>
@@ -41,6 +41,25 @@
                 </div>
             </div>
             {{Form::close()}}
+        </div>
+        <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">削除しますか？</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        {{ $workbook->getTitle() }}
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-light" data-dismiss="modal">キャンセル</button>
+                        <button type="button" class="btn btn-danger">削除</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
