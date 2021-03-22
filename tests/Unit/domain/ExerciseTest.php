450ffbddc0528252cc64780a1f48ceb16fab29f9
Koichi Taura <taura.koichi@gmail.com>
2021-03-22 17:19:57 +0900

    use phpdbg command for coverage report

--- tests/Unit/domain/ExerciseTest.php
+++ tests/Unit/domain/ExerciseTest.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace Tests\Unit\domain;
-use Tests\TestCase;
-use App\Domain\Exercise;
+
 use App\Domain\DomainException;
-use \Mockery as m;
+use App\Domain\Exercise;
+use Tests\TestCase;
 
 class ExerciseTest extends TestCase
 {
@@ -12,12 +12,14 @@ class ExerciseTest extends TestCase
     {
         parent::setUp();
     }
+
     public function tearDown(): void
     {
         parent::tearDown();
     }
 
-    public function testCreate() {
+    public function testCreate()
+    {
         try {
             $exercise = Exercise::create(["question" => "Do you like dog?", "answer" => "yes, I like."]);
             $actual = $exercise->getExerciseDto();
@@ -28,5 +30,14 @@ class ExerciseTest extends TestCase
         }
     }
 
-
+    public function testCreateExceptionWithNullQuestion()
+    {
+        try {
+            Exercise::create(["answer" => "yes, I like."]);
+        } catch (DomainException $e) {
+            self::assertSame('質問が空です。', $e->getMessage());
+        } catch (\Exception $e) {
+            self::fail("予期しない例外が発生しました。" . $e);
+        }
+    }
 }
