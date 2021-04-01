189b26740b8b274786a8b5a1827d86b75c49cdce
Koichi Taura <taura.koichi@gmail.com>
2021-04-01 18:35:48 +0900

    add test for exercise repository

--- tests/Feature/infrastructure/ExerciseRepositoryTest.php
+++ tests/Feature/infrastructure/ExerciseRepositoryTest.php
@@ -3,6 +3,7 @@
 
 namespace Tests\Unit\infrastructure;
 
+use App\Exceptions\DataNotFoundException;
 use App\Infrastructure\ExerciseRepository;
 use Tests\TestCase;
 
@@ -17,5 +18,26 @@ class ExerciseRepositoryTest extends TestCase
         self::assertSame(10, $actual->user_id);
         self::assertSame('Is this a dog?', $actual->question);
         self::assertSame('Yes, it is', $actual->answer);
+    }
+
+    public function testFindByExerciseIdWithOtherUser()
+    {
+        $exercise_repository = new ExerciseRepository();
+        $exercise_domain = $exercise_repository->findByExerciseId('exercise1', 15);
+        $actual = $exercise_domain->getExerciseDto();
+        self::assertSame('exercise1', $actual->exercise_id);
+        self::assertSame(10, $actual->user_id);
+        self::assertSame('Is this a dog?', $actual->question);
+        self::assertSame('Yes, it is', $actual->answer);
+    }
+
+    public function testFindByExerciseIdWithInvalidId()
+    {
+        $exercise_repository = new ExerciseRepository();
+        try {
+            $exercise_repository->findByExerciseId('exercise999', 15);
+        } catch (DataNotFoundException $e) {
+            self::assertSame('Data not found in exercises by id: exercise999', $e->getMessage());
+        }
     }
 }
