a1687ab97591c9e8d3aa63b217a3dac32c6389dd
Koichi Taura <taura.koichi@gmail.com>
2021-04-02 22:50:45 +0900

    add test for workbook repository

--- tests/Feature/infrastructure/WorkbookRepositoryTest.php
+++ tests/Feature/infrastructure/WorkbookRepositoryTest.php
@@ -39,4 +39,34 @@ class WorkbookRepositoryTest extends TestCase
         self::assertTrue($actual instanceof Workbooks);
         self::assertSame(4, $actual->count());
     }
+
+    public function testUpdate()
+    {
+        $workbook_repository = new WorkbookRepository();
+        $workbook_domain = $workbook_repository->findByWorkbookId('test2');
+        $workbook_domain->edit([
+            'title' => 'modified test workbook2',
+            'explanation' => 'this is modified sample workbook no.2'
+        ]);
+        $workbook_repository->update($workbook_domain);
+        $acutal = $workbook_repository->findByWorkbookId('test2');
+        $dto = $acutal->getWorkbookDto();
+        self::assertSame('modified test workbook2', $dto->title);
+        self::assertSame('this is modified sample workbook no.2', $dto->explanation);
+    }
+
+    public function testDelete()
+    {
+        $workbook_repository = new WorkbookRepository();
+        $workbooks = $workbook_repository->findWorkbooks();
+        self::assertSame(4, $workbooks->count());
+        $workbook_repository->delete('test3');
+        $workbooks = $workbook_repository->findWorkbooks();
+        self::assertSame(3, $workbooks->count());
+        try {
+            $workbook_repository->findByWorkbookId('test3');
+        } catch (DataNotFoundException $e) {
+            self::assertSame('Data Not Fount: Invalid Workbook Id.', $e->getMessage());
+        }
+    }
 }
