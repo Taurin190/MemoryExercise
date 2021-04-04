a058c96de231b20b0ad2ca08d910861c7c556fec
Koichi Taura <taura.koichi@gmail.com>
2021-04-04 17:34:07 +0900

    add test for exercise controller

---
+++ tests/Feature/controller/ExerciseControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Tests\Feature\controller;
+
+
+use Tests\TestCase;
+
+class ExerciseControllerTest extends TestCase
+{
+    /**
+     * @dataProvider getTestDataForTestDetail
+     */
+    public function testDetail($uuid, $title)
+    {
+        $user = factory(\App\User::class)->make();
+        $response = $this->actingAs($user)->get(route('exercise.detail', $uuid));
+        $response->assertStatus(200);
+        $response->assertSee($title);
+    }
+
+    public function getTestDataForTestDetail()
+    {
+        return [
+            'exercise-1' => ['exercise1', 'Is this a dog?'],
+            'exercise-2' => ['exercise2', 'Is this a cat?'],
+        ];
+    }
+}
