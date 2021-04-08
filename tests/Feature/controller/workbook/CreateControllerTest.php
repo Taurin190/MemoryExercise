d889613c70c77128f04187efa4dce1872699ee08
Koichi Taura <taura.koichi@gmail.com>
2021-04-09 08:39:46 +0900

    add test case for confirm and complete screen for workbook

--- tests/Feature/controller/workbook/CreateControllerTest.php
+++ tests/Feature/controller/workbook/CreateControllerTest.php
@@ -4,6 +4,7 @@
 namespace Tests\Feature\controller\workbook;
 
 
+use App\Workbook;
 use Tests\TestCase;
 
 class CreateControllerTest extends TestCase
@@ -20,5 +21,38 @@ class CreateControllerTest extends TestCase
         $response = $this->get(route('workbook.create'));
         $response->assertStatus(302);
         $response->assertLocation('/login');
+    }
+
+    public function testConfirm()
+    {
+        $user = factory(\App\User::class)->create();
+        $response = $this->actingAs($user)->post(route('workbook.confirm'), [
+            'title' => 'test workbook 1',
+            'explanation' => 'this is test workbook for test',
+            'exercise_list' => ['exercise1', 'exercise2']
+        ]);
+        $response->assertStatus(200);
+        $response->assertSessionHas('title_create');
+        $response->assertSessionHas('explanation_create');
+        $response->assertSessionHas('exercise_list_create');
+        $user->delete();
+    }
+
+    public function testComplete()
+    {
+        $user = factory(\App\User::class)->create();
+        $response = $this
+            ->actingAs($user)
+            ->withSession([
+                'title_create' => 'test workbook 1',
+                'explanation_create' => 'this is test workbook for test'
+            ])
+            ->post(route('workbook.complete'));
+        $response->assertStatus(200);
+        $response->assertSessionMissing('title_create');
+        $response->assertSessionMissing('explanation_create');
+        $response->assertSessionMissing('exercise_list_create');
+        Workbook::where('title', 'test workbook 1')->delete();
+        $user->delete();
     }
 }
