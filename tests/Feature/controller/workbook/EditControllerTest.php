359d581f3264a66ed54f034e096d0e8dc5d80b29
Koichi Taura <taura.koichi@gmail.com>
2021-04-10 09:40:17 +0900

    add test for workbook edit confirm and complete

--- tests/Feature/controller/workbook/EditControllerTest.php
+++ tests/Feature/controller/workbook/EditControllerTest.php
@@ -35,4 +35,45 @@ class EditControllerTest extends TestCase
         $response = $this->actingAs($user)->get(route('workbook.edit', 'test1'));
         $response->assertStatus(403);
     }
+
+    public function testConfirm()
+    {
+        $user = factory(\App\User::class)->create();
+        $workbook = factory(\App\Workbook::class)->create([
+            'author_id' => $user->getKey()
+        ]);
+        $id = $workbook->getKey();
+        $response = $this->actingAs($user)->post(route('workbook.edit.confirm', $id), [
+            'title' => 'this is modified test title',
+            'explanation' => 'this is modified test explanation',
+        ]);
+        $response->assertStatus(200);
+        $response->assertSessionHas('title_edit');
+        $response->assertSessionHas('explanation_edit');
+        $response->assertSessionHas('exercise_list_edit');
+        $workbook->delete();
+        $user->delete();
+    }
+
+    public function testComplete()
+    {
+        $user = factory(\App\User::class)->create();
+        $workbook = factory(\App\Workbook::class)->create([
+            'author_id' => $user->getKey()
+        ]);
+        $id = $workbook->getKey();
+        $response = $this
+            ->actingAs($user)
+            ->withSession([
+                'title_edit' => 'this is modified test title',
+                'explanation_edit' => 'this is modified test explanation'
+            ])
+            ->post(route('workbook.edit.complete', $id));
+        $response->assertStatus(200);
+        $response->assertSessionMissing('title_edit');
+        $response->assertSessionMissing('explanation_edit');
+        $response->assertSessionMissing('exercise_list_edit');
+        $workbook->delete();
+        $user->delete();
+    }
 }
